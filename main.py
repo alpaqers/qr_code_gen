310d800d989ce223e9fd2c92b9d2bb31902f8830
alpaqers <pawko0202@gmail.com>
2025-04-19 01:33:12 +0200

    changed pillow version in req.txt

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
+    from website import db
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+import os
 from flask_login import LoginManager
-from flask_login import current_user
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'qwerty'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     db.init_app(app)
 
     from .views import views
@@ -20,7 +18,6 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -36,10 +33,3 @@ def create_app():
         return dict(user=current_user)
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        with app.app_context():
-            db.create_all()
-        print('Created Database!')
