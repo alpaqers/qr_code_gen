fd2391c333ef06fa281a639cb6514641f7f72f24
alpaqers <pawko0202@gmail.com>
2025-04-19 02:02:16 +0200

    deleted dbinit endpoint for safety

--- main.py
+++ main.py
@@ -2,9 +2,5 @@ from website import create_app
 
 app = create_app()
 
-from website import db
-
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
